b976836c28d83464634540c7c54854e6d3b3e523
Kuzminih305 <Kuzminih187@gmail.com>
2023-01-04 11:43:33 +0300

    next

--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import s from './FriendMessage.module.css'
+import {friendMessage0} from "../HW1";
 
 // создать тип вместо any и отобразить приходящие данные
-const FriendMessage = (props: any) => {
+export type Message_PropsType = {
+    message: {
+        id: number
+    }
+
+}
+
+
+const FriendMessage = (props: Message_PropsType) => {
     return (
         <div
             id={'hw1-friend-message-' + props.message.id}
@@ -11,7 +20,7 @@ const FriendMessage = (props: any) => {
             <div className={s.friendImageAndText}>
                 <img
                     id={'hw1-friend-avatar-' + props.message.id}
-                    // создаёт студент
+                    src={friendMessage0.user.avatar}
 
                     //
                 />
@@ -20,7 +29,7 @@ const FriendMessage = (props: any) => {
                         id={'hw1-friend-name-' + props.message.id}
                         className={s.friendName}
                     >
-                        {/*создаёт студент*/}
+                        {friendMessage0.user.name}
 
                         {/**/}
                     </div>
@@ -28,7 +37,7 @@ const FriendMessage = (props: any) => {
                         id={'hw1-friend-text-' + props.message.id}
                         className={s.friendMessageText}
                     >
-                        {/*создаёт студент*/}
+                        {friendMessage0.message.text}
 
                         {/**/}
                     </pre>
@@ -38,7 +47,7 @@ const FriendMessage = (props: any) => {
                 id={'hw1-friend-time-' + props.message.id}
                 className={s.friendTime}
             >
-                {/*создаёт студент*/}
+                {friendMessage0.message.time}
 
                 {/**/}
             </div>
