ea4cdb27bb912ad647902910b34fb4b35d04c5c0
Kuzminih305 <Kuzminih187@gmail.com>
2023-03-23 19:20:52 +0300

    goodjob hw 12

--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -1,14 +1,28 @@
 const initState = {
     themeId: 1,
 }
+export type StateType = {
+    themeId: number
+}
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state:StateType = initState, action: ChangeThemeIdType): StateType => { // fix any
     switch (action.type) {
         // дописать
-
+        case 'SET_THEME_ID' :
+            return {themeId: action.payload.id}
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ChangeThemeIdType = ReturnType<typeof changeThemeId>
+
+export const changeThemeId = (id: number) => {
+    return {
+        type: 'SET_THEME_ID',
+        payload: {
+            id
+        }
+    }as const
+}
+
